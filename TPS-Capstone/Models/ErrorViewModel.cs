220e38beaf3cbac11cba7ea97405c5beaed528ab
Joshbidaru <162331923+Joshbidaru@users.noreply.github.com>
2024-08-31 18:07:43 +0800

    UI
    Login & Registration and Services Page

--- TPS-Capstone/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TPS_Capstone.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
